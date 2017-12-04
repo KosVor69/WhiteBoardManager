c11e45e0e7883b69801bb3b607e1a197e06be1d8
Konstantin Vorobyev <Konstantin_Vorobyev@jabil.com>
2017-12-04 15:46:03 +0300

    Готовая шапка

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import moment = require('moment');
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'app';
+date = moment().format('LTS');
 }
